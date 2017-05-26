e4596b0f204f40d59175c437dcb43dc50ab2c673
Paula <pmuldoon@umich.edu>
2017-05-26 16:35:56 +0100

    styles.css added

--- src/headlines-view-model.js
+++ src/headlines-view-model.js
@@ -7,7 +7,7 @@
     var display = "<ul>";
     this._headlines.forEach(function(headline) {
       display += ("<li><div>" + headline.webTitle + "<br>" +
-                  "<a href=" + headline.webUrl + ">Source</a>" + 
+                  "<a href=" + headline.webUrl + ">Source</a>" +
                   "</div></li><br>");
     });
     return (display + "</ul>");
