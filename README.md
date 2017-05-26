b7cd19e16def85957e2d7ec076b49daf9e2efb0d
milesillsley <milesillsley@hotmail.co.uk>
2017-05-26 11:38:23 +0100

    Extract json object from XML http response

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ITs theNEWS
+
+
+google searches:
+
+js flatten --> js flatten string -->js flatten object out of string -->js json object out of string !!!
--- src/api-controller.js
+++ src/api-controller.js
@@ -3,9 +3,10 @@ function loadAPI() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      headlines.recieveAPIrequest(this);
-      document.getElementById("headlines").innerHTML = headlines.getAPI();
-      console.log(this);
+      // headlines.recieveAPIrequest(this);
+      var obj = this.response;
+      document.getElementById("headlines").innerHTML = JSON.parse(this.responseText);
+      console.log(JSON.parse(this.responseText));
     }
   };
   xhttp.open("GET", "https://content.guardianapis.com/search?api-key=87025228-f81d-4609-8394-ec517d29329a", true);
